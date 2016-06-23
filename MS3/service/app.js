e166c1c713615d6f94b640d94a06ae6838074d4b
Markusernst <markusernst92@web.de>
2016-06-23 19:50:07 +0200

    fixed waterneed on service

--- MS3/service/app.js
+++ MS3/service/app.js
@@ -17,49 +17,57 @@ require("./api/duenger/import").register(app, db);
 
 // Calculate amount of needed water (pre-Weather-API) 
 app.get('/waterneed/:uid/:id', function(req,res){
-        var mphu=0;
-        var phu=0;
-        var n=0;
-        var pid=1;
+        var mphu;
+        var phu;
+        var n;
+        var pid;
         var obj={};
         var obj2={};
+
+		function calculateWaterNeed(a, b, c){
+		        if (a < b){
+		            c = b - a;
+		            return c;  
+		        }
+		        else {
+		        	c = 0;
+		        	console.log('Keine Bewaesserung noetig.')
+		            return c;
+		        }
+		}
+
         db.get('uu:'+req.params.uid+':measuredPlant:'+req.params.id, function(err, rep){
 			if(rep) {
-				//res.type('json').send(rep);
                 //console.log(JSON.parse(rep));
                 obj = JSON.parse(rep);
                 mphu = obj.bodenfeuchtigkeit;
-                //pid = parseInt(obj.plantid); //dynamic does not work yet
-                console.log(mphu);
+                pid = obj.plantid;
+
+			db.get('plant:'+ pid, function(err, rep) {
+				if (rep) {
+    	            obj2 = JSON.parse(rep);
+    	            phu = obj2.bodenfeuchtigkeit;
+				}
+				else {
+					res.status(404).type("text").send("Die Pflanze mit der ID " + pid + " wurde nicht gefunden");
+				}
+
+				n = calculateWaterNeed(mphu, phu, n)
+				console.log(n);
+				var Stringresponse = {'wasserbedarf': n, 'plantid': pid, 'measuredPlantid': req.params.id};
+				var JSONresponse = JSON.stringify(Stringresponse);
+				console.log(JSONresponse);
+
+        		res.type("json").send(JSONresponse);
+
+			});
+
+
 			}
 			else {
 				res.status(404).type('text').send('Die zugewiesene Pflanze mit der ID '+req.params.id+' wurde nicht gefunden');
 			}
 		});
-        
-        db.get('plant:'+ pid, function(err, rep) {
-			if (rep) {
-				//res.type("json").send(rep);
-                obj2 = JSON.parse(rep);
-                phu = obj2.bodenfeuchtigkeit;
-                console.log(phu);
-			}
-			else {
-				res.status(404).type("text").send("Die Pflanze mit der ID " + pid + " wurde nicht gefunden");
-			}
-		});
-        
-        function calculateWaterNeed(a, b, c){
-        if (a < b){
-            c = b - a;
-            return c;  
-        }
-        else {
-            return console.log('Keine Bewaesserung noetig.');
-        }
-        }
-        n = calculateWaterNeed(mphu, phu, n)
-        res.send(n);
 });
  
         
